03196de60d0bfa6799b41fde0fcd268e1e2f4ed0
jhtr0226 <johanncraft17@gmail.com>
2024-09-25 20:17:43 -0600

    7

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
@@ -19,13 +26,6 @@ app.use(static)
 app.get("/", function (req, res) {
   res.render("index", {title: "Home"})
 })
-
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", ".layouts/layout")
 
 /* ***********************
  * Local Server Information
